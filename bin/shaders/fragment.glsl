df0766a991e22cbba6ae2e2c6fd7a2f24643f3f9
Thabo Twala <thabonjabulotwala@gmail.com>
2024-09-13 05:16:17 +0200

    Twas changed long ago don.t rem the diffs

--- bin/shaders/fragment.glsl
+++ bin/shaders/fragment.glsl
@@ -4,10 +4,11 @@ out vec4 FragColor;
 
 //in vec3 f_colour;
 in vec2 TexCoord;
-
+in vec2 map;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-
+uniform float mapx;
+uniform float mapy;
 void main(){
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+	FragColor = mix(texture(texture1, vec2(TexCoord.x * mapx, TexCoord.y * mapy)), texture(texture2, vec2(1.0f - TexCoord.x,TexCoord.y)), 0.0);
 }
